043137c5012f90caa8e978dd307942ce8de942ce
Tapaj Kumar Das <tapajkumardas@gmail.com>
2022-03-28 02:43:03 +0530

    Adds logging

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 "use strict";
 
+const rfs = require("rotating-file-stream");
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 
 const indexRoutes = require("./routes/index.routes");
 
@@ -9,12 +12,32 @@ const indexRoutes = require("./routes/index.routes");
 // TODO: Implement error handling when .env file is not found
 dotenv.config({ debug: true });
 
+// Create a write stream, in append mode
+const accessLogStream = rfs.createStream("access.log", {
+    interval: "1d",
+    path: path.join(__dirname, "..", "logs"),
+});
+
+const errorLogStream = rfs.createStream("error.log", {
+    interval: "1d",
+    path: path.join(__dirname, "..", "logs"),
+});
+
 const app = express();
 const PORT = process.env.PORT;
 
 // Middlewares
 // TODO: Add a default starting route to all routes, like /api/v1
 app.use(express.json());
+app.use(morgan("combined", { stream: accessLogStream }));
+app.use(
+    morgan("combined", {
+        skip: (req, res) => {
+            return res.statusCode < 400;
+        },
+        stream: errorLogStream,
+    })
+);
 
 // Routes
 app.use("/api", indexRoutes);
